c606520aa7801bbb34e960467149b79305edf10b
Mike Griffiths <mike.griffiths@reckless.agency>
2017-01-17 08:44:23 +0000

    Written and implemented an OMDB wrapper package, and created database migration for movies
    Signed-off-by: Mike Griffiths <mike.griffiths@reckless.agency>

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DigiPig\MovieDB;
 
 class HomeController extends Controller
 {
@@ -13,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        //$this->middleware('auth');
     }
 
     /**
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
+        $MoviesArray = \MovieDB::getMovie('tt2937696');
+        dd($MoviesArray);
         return view('home');
     }
 }
